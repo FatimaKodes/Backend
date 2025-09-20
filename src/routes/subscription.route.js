63774f44fb7773ca89117ebe0c4396680d25041e
FatimaKodes <bismaalishaikh632@gmail.com>
2025-09-20 11:38:39 -0700

    Implement subscription management features including toggling subscriptions, fetching subscriber counts, and retrieving subscribed channels; add corresponding routes

---
+++ src/routes/subscription.route.js
@@ -0,0 +1,18 @@
+import {Router} from 'express'
+import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { getSubscribedChannels, getUserChannelSubscribers, 
+        toggleSubscription } from '../controllers/subscription.controller.js';
+
+const router = Router();
+router.use(verifyJWT)
+
+router.route('/:channelId')
+.post(toggleSubscription)
+
+router.route('/:channelId')
+.get(getUserChannelSubscribers)
+
+router.route('/get/:subscriberId')
+.get(getSubscribedChannels)
+
+export default router
